1a3f2825979c8feca8b2f01080703ef46b929037
Tarek <tarekhossenpc@gmail.com>
2026-07-01 18:46:19 +0600

    successfully read operation done among the crud operation

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(express.json());
 //simpleCrudUser
 //zMS4u1FdoSGq1Xr6
 //Uniform Resource identifier or Connection String
-const uri = "mongodb+srv://simpleCrudUser:zMS4u1FdoSGq1Xr6@tarek-hossen.t4byzjz.mongodb.net/?appName=Tarek-Hossen"
+const uri =
+  "mongodb+srv://simpleCrudUser:zMS4u1FdoSGq1Xr6@tarek-hossen.t4byzjz.mongodb.net/?appName=Tarek-Hossen";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,26 +22,37 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 app.get("/", (req, res) => {
   res.send("simple crud server");
 });
 
-async function run(){
-  try{
-    await client.connect()
+async function run() {
+  try {
+    await client.connect();
+    const usersDB = client.db("usersDB");
+    const usersCollection = usersDB.collection("usersCollection");
+
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //database related apis here
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      console.log("hitting the server console", newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
 
     console.log("✅ Connected successfully!");
     // console.log("connected to Mongodb!")
-    
+  } finally {
   }
-  finally{
-
-  }
-
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`simple crud server is loading on port ${port} `);
